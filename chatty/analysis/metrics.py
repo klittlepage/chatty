dac0eece760e568509fe56a17380c5a8368f89c1
Kelly Littlepage <kelly.littlepage@gmail.com>
2018-01-10 13:42:44 -0700

    Refactored analysis.metrics for easier reuse

--- chatty/analysis/metrics.py
+++ chatty/analysis/metrics.py
@@ -17,52 +17,83 @@ class Metric(object):
         val = self.compute(messages, sentiment)
         return self.format_str.format(val)
 
-DEFAULT_METRICS = [
-    Metric('Message Count',
-           lambda messages, sentiment: len(messages),
-           "{:,}"),
-    Metric('Word Count',
-           lambda messages, sentiment: sum(len(x) for x in messages),
-           "{:,}"),
-    Metric('Avg Message Word Count',
-           lambda messages, sentiment: avg_message_words(messages),
-           "{0:.2f}"),
-    Metric('Avg Message Word Count (w/o Stop Words)',
-           lambda messages, sentiment: \
-               avg_message_words(filter_stopwords(messages)),
-           "{0:.2f}"),
-    Metric('Avg Message Character Count',
-           lambda messages, sentiment: avg_message_chars(messages),
-           "{0:.2f}"),
-    Metric('Avg Message Character Count (w/o Stop Words)',
-           lambda messages, sentiment:
-           avg_message_chars(filter_stopwords(messages)),
-           "{0:.2f}"),
-    Metric('Longest Message',
-           lambda messages, sentiment: \
-               longest_message(messages),
-           "{:,}"),
-    Metric('Distinct Words',
-           lambda messages, sentiment: len(vocab(messages)),
-           "{:,}"),
-    Metric('Distinct Stems',
-           lambda messages, sentiment: len(vocab(stem(messages))),
-           "{:,}"),
+MESSAGE_COUNT = Metric('Message Count',
+                       lambda messages, sentiment: len(messages),
+                       "{:,}")
+
+WORD_COUNT = Metric('Word Count',
+                    lambda messages, sentiment: sum(len(x) for x in messages),
+                    "{:,}")
+
+AVG_WORD_COUNT = Metric('Avg Message Word Count',
+                        lambda messages, sentiment: avg_message_words(messages),
+                        "{0:.2f}")
+
+AVG_WORD_COUNT_WO_STOP = Metric('Avg Message Word Count (w/o Stop Words)',
+                                lambda messages, sentiment: \
+                                avg_message_words(filter_stopwords(messages)),
+                                "{0:.2f}")
+
+AVG_CHAR_COUNT = Metric('Avg Message Character Count',
+                        lambda messages, sentiment: avg_message_chars(messages),
+                        "{0:.2f}")
+
+AVG_CHAR_COUNT_WO_STOP = Metric('Avg Message Character Count (w/o Stop Words)',
+                                lambda messages, sentiment:
+                                avg_message_chars(filter_stopwords(messages)),
+                                "{0:.2f}")
+
+LONGEST_MESSAGE = Metric('Longest Message',
+                         lambda messages, sentiment: longest_message(messages),
+                         "{:,}")
+
+DISTINCT_WORDS = Metric('Distinct Words',
+                        lambda messages, sentiment: len(vocab(messages)),
+                        "{:,}")
+
+DISTINCT_STEMS = Metric('Distinct Stems',
+                        lambda messages, sentiment: len(vocab(stem(messages))),
+                        "{:,}")
+
+DISTINCT_ENGLISH_WORDS = \
     Metric('Distinct English Words',
            lambda messages, sentiment: len(vocab(filter_english(messages))),
-           "{:,}"),
-    Metric('Distinct Emojis',
-           lambda messages, sentiment: len(emojis(messages)),
-           "{:,}"),
+           "{:,}")
+
+DISTINCT_EMOJIS = Metric('Distinct Emojis',
+                         lambda messages, sentiment: len(emojis(messages)),
+                         "{:,}")
+
+EMOJI_COUNT = \
     Metric('Emoji Count',
            lambda messages, sentiment: sum(x[1] for x in emojis(messages)),
-           "{:,}"),
+           "{:,}")
+
+PCT_NEGATIVE_SENTIMENT = \
     Metric('Pct Negative Sentiment',
            lambda messages, sentiment: \
-               pct_sentiment(sentiment, lambda x: x < -.5),
-           "{0:.0f}%"),
+           pct_sentiment(sentiment, lambda x: x < -.5),
+           "{0:.0f}%")
+
+PCT_POSITIVE_SENTIMENT = \
     Metric('Pct Positive Sentiment',
            lambda messages, sentiment: \
-               pct_sentiment(sentiment, lambda x: x > .5),
+           pct_sentiment(sentiment, lambda x: x > .5),
            "{0:.0f}%")
+
+DEFAULT_METRICS = [
+    MESSAGE_COUNT,
+    WORD_COUNT,
+    AVG_WORD_COUNT,
+    AVG_WORD_COUNT_WO_STOP,
+    AVG_CHAR_COUNT,
+    AVG_CHAR_COUNT_WO_STOP,
+    LONGEST_MESSAGE,
+    DISTINCT_WORDS,
+    DISTINCT_STEMS,
+    DISTINCT_ENGLISH_WORDS,
+    DISTINCT_EMOJIS,
+    EMOJI_COUNT,
+    PCT_NEGATIVE_SENTIMENT,
+    PCT_POSITIVE_SENTIMENT
 ]
